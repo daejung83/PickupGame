42356b53c33b3b06b28ed050d1c6c23743bc25b1
changhe3 <hechang1997>
2017-11-29 17:33:00 -0600

    Add local authentication

--- backend/routers/index.js
+++ backend/routers/index.js
@@ -1,13 +1,20 @@
 import users from './users'
 import home from './home'
 import login from './login'
+import logout from './logout'
+import register from './register'
 import {Router} from 'express'
+import user from './user'
 
-const router = Router();
 
-router.use('/users', users);
+export default function (router, passport) {
+    router.use('/users', users(new Router(), passport));
 
-home(router.route('/'));
-login(router.route('/login'));
+    home(router.route('/'));
+    login(router.route('/login'), passport);
+    logout(router.route('/logout'), passport);
+    register(router.route('/register'), passport);
+    user(router.route('/user'));
 
-export default router;
+    return router
+}
--- backend/routers/login.js
+++ backend/routers/login.js
@@ -1,5 +1,11 @@
-export default function login(route) {
-    route.post((req, res) => {
-
-    });
+export default function(route, passport) {
+    route.post(
+        passport.authenticate('local-signin'),
+        (req, res) => {
+            console.log(req.isAuthenticated());
+            res.status(200).json({
+                user: req.user.email
+            });
+        }
+    );
 }
