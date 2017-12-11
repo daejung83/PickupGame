5f14de8cce87b71649e7f86a78349e0f404804bb
changhe3 <hechang1997>
2017-12-11 16:45:49 -0600

    fix rating

--- backend/routers/users/id.js
+++ backend/routers/users/id.js
@@ -1,11 +1,12 @@
 import isLoggedIn from "../../utils/isLoggedIn";
-import User from "../../schemas/User";
+import User, {profileInfo} from "../../schemas/User";
 
 export default function (route) {
     // GET /api/users/:id
+    // get profiles of a user
     route.get(isLoggedIn, async (req, res) => {
         const id = req.params.id;
-        const user = await User.findById(id, "name rating numberRated preferredSport").exec();
+        const user = await User.findById(id, profileInfo).exec();
         if (user) return res.status(200).json(user);
         else return res.status(404).json({message: "User ID not found"});
     });
@@ -15,7 +16,7 @@ export default function (route) {
     // rating should be between 0 and 1
     route.post(isLoggedIn, async (req, res) => {
         if (req.user._id.equals(req.params.id)) return res.status(403).json({message: "You cannot rate yourself!"});
-        const user = await User.findById(req.params.id, "name rating numberRated preferredSport").exec();
+        const user = await User.findById(req.params.id, profileInfo).exec();
         if (!user) return res.status(404).json({message: "User ID not found"});
         if (!req.body || !req.body.rating) return res.status(400).json({message: "body.rating is required"});
         const rating = parseFloat(req.body.rating);
