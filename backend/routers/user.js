0f923eb7c871e15fe3ccde97dbd5f5dc6dc30e4e
Emre Ulusoy <emreulusoy95@hotmail.com>
2017-12-10 18:35:46 -0600

    PUT /api/users/:id

--- backend/routers/user.js
+++ backend/routers/user.js
@@ -4,8 +4,31 @@ export default function (route) {
 
     // GET /api/user
     // return an json object with user being null if user is not logged in, otherwise return the object with the user
-    route.get(isLoggedIn, (req, res) => {
+    route.get(isLoggedIn, async (req, res) => {
         if (req.isAuthenticated()) return res.status(200).json({ user: req.user, message: "Welcome!"});
         else return res.status(200).json({user: null, message: "You are not logged in."});
+    });
+
+    // PUT /api/user
+    route.put(isLoggedIn, async (req, res) => {
+        const user = await User.findById(req.user.id).exec();
+
+        if(req.body.name) user.name = req.body.name;
+        if(req.body.email) user.email = req.body.email;
+        if(req.body.preferredSport) user.preferredSport = req.body.preferredSport;
+        await user.save();
+        req.logIn(user, function(error) {
+            if(error){
+                return res.status(500).json({message: "Internal error."})
+            }
+        });
+        return res.status(200).json({message: "User updated.", data: user});
+    });
+
+    // POST /api/user
+    route.post(isLoggedIn, async (req, res) => {
+        const user = await User.findById(req.params.id).exec();
+        if (user._id === req.user._id) return res.status(401).json({message: "Can't rate yourself."});
+
     });
 }
