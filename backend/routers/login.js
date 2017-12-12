a3011bf0e26b1e6503fc0c3a7a8bbe7e6ecccebb
changhe3 <hechang1997>
2017-12-12 17:55:33 -0600

    fix login and register

--- backend/routers/login.js
+++ backend/routers/login.js
@@ -1,10 +1,16 @@
 export default function(route, passport) {
     route.post(
-        passport.authenticate('local-signin'),
-        (req, res) => {
-            res.status(200).json({
-                user: req.user
-            });
+        (req, res, next) => {
+            passport.authenticate('local-signin', (err, user, info) => {
+                if (err) next(err);
+                if (!user) {
+                    return res.status(401).json(info);
+                }
+                req.logIn(user, err => {
+                    if (err) return next(err);
+                    else return res.status(200).json({user: user});
+                });
+            })(req, res, next);
         }
     );
 }
--- backend/routers/register.js
+++ backend/routers/register.js
@@ -1,9 +1,17 @@
 export default function (route, passport) {
     // if (passport) console.log("Passport");
     route.post(
-        passport.authenticate('local-signup'),
-        (req, res) => {
-            res.status(201).json({user: req.user});
+        (req, res, next) => {
+            passport.authenticate('local-signup', (err, user, info) => {
+                if (err) next(err);
+                if (!user) {
+                    return res.status(401).json(info);
+                }
+                req.logIn(user, err => {
+                    if (err) return next(err);
+                    else return res.status(200).json({user: user});
+                });
+            })(req, res, next);
         }
     );
 }
